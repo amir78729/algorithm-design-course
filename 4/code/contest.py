8a23c5436d179032de0cf4fcfb9462a2c5b4de72
amir78729 <amir78729@gmail.com>
2020-12-23 23:03:12 +0330

    contest 93%

--- 4/code/contest.py
+++ 4/code/contest.py
@@ -8,11 +8,13 @@ single person, one at a time"""
 # f[] --> An array that contains finish time of all activities 
 
 def printMaxActivities( ): 
-	print ("The following activities are selected")
-
+	global res
+	# print ("The following activities are selected") 
+    # res = 0
 	# The first activity is always selected 
 	i = 0
-	print (i)
+    
+	# print (i)
 
 	# Consider rest of the activities 
 	for j in range(k): 
@@ -20,30 +22,45 @@ def printMaxActivities( ):
 		# If this activity has start time greater than 
 		# or equal to the finish time of previously 
 		# selected activity, then select it 
-		if s[j] >= f[i]: 
-			print (j,"}") 
+        # if s[j] >= f[i]: 
+		if s[j][0] >= s[i][1]: 
+            # res = 1 + res
+			res += 1
 			i = j 
+            
+
+
+# take second element for sort
+def takeSecond(elem):
+    return elem[1]
 
 # Driver program to test above function 
 s = [] 
 f = [] 
 
-
+res = 0
 # printMaxActivities(s, f) 
 
 n, k = input().split()
 n, k = int(n),int(k)
 for index in range(k):
     l, r = input().split()
-    s.append(int(l))
-    f.append(int(r))
+    # s.append(int(l))
+    # f.append(int(r))
+    tmp = []
+    s.append(tmp)
+    s[index].append(int(l))
+    s[index].append(int(r))
+
+s.sort(key=takeSecond)
 # print(k)
 # print(s)
 # print(f)
 if n <= k:
-    printMaxActivities(s, f) 
+    printMaxActivities() 
+    print("YES")
 else:
-    print("FALSE")
+    print("NO")
 
 
 # This code is contributed by Nikhil Kumar Singh 
