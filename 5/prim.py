e96e3e648472d7f548133cd41f60c26b7b7d9c30
amir78729 <amir78729@gmail.com>
2021-01-07 18:43:50 +0330

    renaiming files

--- 5/prim.py
+++ 5/prim.py
@@ -4,10 +4,9 @@ import operator as op
 from functools import reduce
 import time
 from collections import defaultdict
-import sys # Library for INT_MAX 
+import sys 
   
 class Graph(): 
-  
     def __init__(self, vertices): 
         self.V = vertices 
         self.graph = [[0 for column in range(vertices)]  for row in range(vertices)] 
@@ -15,64 +14,32 @@ class Graph():
     # A utility function to print the constructed MST stored in parent[] 
     def printMST(self, parent): 
         result = []
-        print ("Edge \tWeight")
         for i in range(1, self.V): 
             result.append([parent[i], i, self.graph[i][ parent[i] ] ])
             # print (parent[i], "-", i, "\t", self.graph[i][ parent[i] ] )
         return result
-  
-    # A utility function to find the vertex with  
-    # minimum distance value, from the set of vertices  
-    # not yet included in shortest path tree 
-    def minKey(self, key, mstSet): 
-  
-        # Initilaize min value 
+
+    def min_key(self, key, mstSet): 
         min = sys.maxsize 
-  
         for v in range(self.V): 
             if key[v] < min and mstSet[v] == False: 
                 min = key[v] 
                 min_index = v 
-  
         return min_index 
-  
-    # Function to construct and print MST for a graph  
-    # represented using adjacency matrix representation 
+ 
     def primMST(self): 
-  
-        # Key values used to pick minimum weight edge in cut 
         key = [sys.maxsize] * self.V 
         parent = [None] * self.V # Array to store constructed MST 
-        # Make key 0 so that this vertex is picked as first vertex 
         key[0] = 0 
         mstSet = [False] * self.V 
-  
-        parent[0] = -1 # First node is always the root of 
-  
+        parent[0] = -1 
         for c in range(self.V): 
-  
-            # Pick the minimum distance vertex from  
-            # the set of vertices not yet processed.  
-            # u is always equal to src in first iteration 
-            u = self.minKey(key, mstSet) 
-  
-            # Put the minimum distance vertex in  
-            # the shortest path tree 
+            u = self.min_key(key, mstSet) 
             mstSet[u] = True
-  
-            # Update dist value of the adjacent vertices  
-            # of the picked vertex only if the current  
-            # distance is greater than new distance and 
-            # the vertex in not in the shotest path tree 
             for v in range(self.V): 
-  
-                # graph[u][v] is non zero only for adjacent vertices of m 
-                # mstSet[v] is false for vertices not yet included in MST 
-                # Update the key only if graph[u][v] is smaller than key[v] 
                 if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]: 
                         key[v] = self.graph[u][v] 
                         parent[v] = u 
-  
         result = self.printMST(parent) 
         return result
 
@@ -85,16 +52,11 @@ def distance(lat1,lon1,lat2,lon2):
 	a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(dlon/2) * math.sin(dlon/2)
 	return 2000 *r * math.atan2(math.sqrt(a),math.sqrt(1-a)) # distance in meters
 
-
 class City:
     def __init__(self,name, x, y):
         self.name = name
         self.x = x
         self.y = y
-
-
-
-		
 
 def brute_force():
     global cities
